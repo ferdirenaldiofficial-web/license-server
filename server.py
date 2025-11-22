869617d02883030e4034f1333c1ae7f22d5b106b
ferdirenaldiofficial-web <ferdirenaldiofficial@gmail.com>
2025-11-22 15:26:12 +0700

    Update server.py

--- server.py
+++ server.py
@@ -66,3 +66,8 @@ def deactivate(req: DeactivateReq):
         raise HTTPException(status_code=403, detail="HWID mismatch")
     doc_ref.update({"active": False, "hwid": ""})
     return {"status": "ok"}
+    if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 3000))   # Railway pakai PORT environment
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
+
